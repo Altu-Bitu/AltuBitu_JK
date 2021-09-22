2e1d859fb73d797160a333c6e4bdf61c67622cc4
jk0527 <78495599+jk0527@users.noreply.github.com>
2021-09-23 00:30:27 +0900

    [Number Theory & Combinations] 9월 23일
    에라토스테네스의 체

--- 9월 17일 - 정수론 및 조합론/6588.cpp
+++ 9월 17일 - 정수론 및 조합론/6588.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
 
 using namespace std;
 
-bool isPrime(int n)
-{
-	for (int i = 2; i <= sqrt(n); i++)
-		if (n % i == 0)
-			return false;
-	return true;
+const int SIZE = 1000000;
+
+// 소수 경로 저장해서 리턴하는 함수
+vector<int> isPrime() {
+	vector<int> prime(SIZE + 1, 0);
+	// 먼저 모든 수를 소수라 가정하고, i번째 인덱스에 i값 저장
+	for (int i = 2; i <= SIZE; i++)
+		prime[i] = i;
+
+	// 소수 판별
+	for (int i = 2; i <= sqrt(SIZE); i++) {
+		if (prime[i] == i) { //소수라면
+			for (int j = i * i; j <= SIZE; j += i) { // 배수에 소수(i) 저장
+				if (prime[j] == j) { // 저장된 소수가 없을 경우만
+					prime[j] = i; // 소수 저장
+				}
+			}
+		}
+	}
+
+	return prime;
 }
 
 int main()
@@ -18,11 +34,12 @@ int main()
 	cin.tie(0); cout.tie(0);
 	int n;
 	cin >> n;
+	vector<int> prime = isPrime();
 	while (n)
 	{
-		for (int i = 3; i <= n/2; i+=2)
+		for (int i = 3; i <= n / 2; i += 2)
 		{
-			if (isPrime(i) && isPrime(n - i))
+			if (prime[i] == i && prime[n-i] == n-i)
 			{
 				cout << n << " = " << i << " + " << n - i << '\n';
 				break;
