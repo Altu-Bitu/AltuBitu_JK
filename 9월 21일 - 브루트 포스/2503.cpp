5003a24d90a45ca7c6bd3ddd7102ed8f3b4456e2
jk0527 <78495599+jk0527@users.noreply.github.com>
2021-09-26 18:04:42 +0900

    [Brute Force] 9월 26일

--- 9월 21일 - 브루트 포스/2503.cpp
+++ 9월 21일 - 브루트 포스/2503.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 int n;
 vector<vector<int>> v(3, vector<int>(n, 0));
 
-int check(int num[])
+bool check(string num)
 {
-	int val = 1;
+	bool val = true;
 	for (int j = 0; j < v[0].size(); j++)
 	{
 		int s_cnt = 0, b_cnt = 0;
-		int guess_num[3];
-		guess_num[0] = v[0][j] / 100; guess_num[1] = (v[0][j]/10)%10; guess_num[2] = v[0][j] % 10;
+		string guess_num = to_string(v[0][j]);
 		for (int k = 0; k < 3; k++)
 		{
 			for (int p = 0; p < 3; p++)
@@ -25,7 +25,7 @@ int check(int num[])
 			}
 		}
 		if (s_cnt != v[1][j] || b_cnt != v[2][j])
-			val = 0;
+			val = false;
 	}
 	return val;
 }
@@ -43,11 +43,9 @@ int main()
 	}
 	for (int i = 111; i <= 999; i++)
 	{
-		// 각 자리의 수 계산
-		int num[3];
-		num[0] = i / 100; num[1] = (i/10)%10; num[2] = i % 10;
+		string num = to_string(i);
 		// 숫자에 0이 들어갈 때
-		if (num[1] == 0 || num[2] == 0)
+		if (num[1] == '0' || num[2] == '0')
 			continue;
 		// 중복되는 숫자 있을 때
 		if (num[0] == num[1] || num[1] == num[2] || num[0] == num[2])
