56e92a320dda0a74a7c65f82ed396cd618c48642
jk0527 <78495599+jk0527@users.noreply.github.com>
2021-10-09 03:26:16 +0900

    [Binary Search] 10월 9일

--- 10월 8일 - 이분 탐색/2342.cpp
+++ 10월 8일 - 이분 탐색/2342.cpp
@@ -33,12 +33,12 @@ int lowerSearch(int left, int right, int target)
 		int mid = (left + right) / 2;
 		int blueray = cut(mid, target); // 블루레이의 크기: mid
 		// 블루레이 최소 크기 구하기
-		if (blueray <= target) // m개 이하의 블루레이에 담을 수 있는 경우
+		if (blueray <= target) // m 개 이하의 블루레이에 담을 수 있는 경우
 		{
 			ans = mid;
 			right = mid - 1;
 		}	
-		else if (blueray > target) // m개보다 많은 블루레이가 필요한 경우
+		else if (blueray > target) // m 개보다 많은 블루레이가 필요한 경우
 			left = mid + 1;
 	}
 	return ans;
